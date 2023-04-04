8bbdd5d05f6c0daf3048feeacaf1d5c022c0f0ec
thiagodorea <thiago.dorea@hotmail.com>
2023-04-04 00:40:28 -0300

    lista os times

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,14 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 import './styles.css'
 
-const Card = () => {
+const Card = (props) => {
+    const { name, image } = props.dados;
+    
     return(
         <div className="card-container">
             <div className="card-container-icon" >
                 <FontAwesomeIcon icon={faCircleExclamation} className="card-icon" />
             </div>
-            <img src='https://logodownload.org/wp-content/uploads/2016/09/flamengo-logo-0.png' alt='{titulo}' className="card-image" />
-            <h3 className="card-title">Flamengo</h3>
+            <img src={image} alt={name} className="card-image" />
+            <h3 className="card-title">{name}</h3>
         </div>
     )
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,13 +1,43 @@
-import Card from "../../components/card/card";
-import BaseTemplate from "../../templates/base-template";
 import "./styles.css";
 
+import Card from "../../components/card/card";
+import BaseTemplate from "../../templates/base-template";
+import api from "../../service/api";
+import { useEffect, useState } from "react";
+
 const Home = () => {
+
+    const [times, setTimes] = useState([]);
+
+    const getTimes = async () => {
+        try {
+            const response = await api.get("/teams", {
+                headers: {
+                    Authorization: localStorage.getItem("@times_token"),
+                },
+            })
+            setTimes(response.data);
+        } catch (error) {
+            alert(error);
+        }
+    }
+    
+    useEffect(() => {
+        getTimes();
+    },[])
+
+
     return (
         <BaseTemplate>
             <div className="list-container">
                 <div className="list-items">
-                    <Card />
+                    {
+                        times.map((time) =>(
+                            <div key={time._id}>
+                                <Card dados={time} />
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </BaseTemplate>
